044e321fc2e9a892ff150a0b7adb20dcf53655ef
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-09-19 17:39:17 +0100

    init commit

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjm5UYJYUrAnAzCmHpVNvrIYpXkMIY39s",
+  authDomain: "fireblog-backend.firebaseapp.com",
+  projectId: "fireblog-backend",
+  storageBucket: "fireblog-backend.appspot.com",
+  messagingSenderId: "827407635082",
+  appId: "1:827407635082:web:68798782fda91845f4d373",
+};
