d37509b4c3480503369718274a4c5f66fdb8dc00
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-09-21 15:08:09 +0100

    Created blog cards for homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,16 +6,30 @@
       v-for="(post, index) in samplePostBlog"
       :key="index"
     />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCards
+            :post="post"
+            v-for="(post, index) in sampleBlogCards"
+            :key="index"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BlogPost from "../components/BlogPost.vue";
+import BlogCards from "../components/BlogCard.vue";
 
 export default {
   name: "Home",
   components: {
     BlogPost,
+    BlogCards,
   },
   data() {
     return {
@@ -64,3 +78,44 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
+  }
+}
+</style>
